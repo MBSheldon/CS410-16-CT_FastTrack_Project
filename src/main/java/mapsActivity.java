68327fa94ae4e6e2933971ae9521352afa1f35b7
danielbereza50 <danielbereza50@gmail.com>
2016-11-06 00:18:42 -0400

    Create mapsActivity.java

---
+++ src/main/java/mapsActivity.java
@@ -0,0 +1,33 @@
+
+package comdanielbereza50cs410_16_ct_fasttrack_project.httpsgithub.ct_fasttrack_project;
+
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng CT = new LatLng(41.763710, -72.685097);
+        mMap.addMarker(new MarkerOptions().position(CT).title("Marker in Connecticut"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(CT));
+    }
+}
